451de0311a03d809345b48494ce2915591999171
dchenk <dcherchenko@gmail.com>
2018-05-07 19:43:54 -0700

    added tests for putMint64, putMint64, putMint32, and getMint32

--- msgp/integers_test.go
+++ msgp/integers_test.go
@@ -4,6 +4,42 @@ import (
 	"testing"
 )
 
+func TestPutGetInt64(t *testing.T) {
+
+	nums := []int64{0, -4, 235, -56475462, 23423423525, -9223372036854775808, 9223372036854775807}
+
+	scratch := make([]byte, 9)
+
+	for _, i := range nums {
+
+		putMint64(scratch, i)
+		got := getMint64(scratch)
+		if got != i {
+			t.Errorf("put in %d but got out %d", i, got)
+		}
+
+	}
+
+}
+
+func TestPutGetInt32(t *testing.T) {
+
+	nums := []int32{0, -4, 235, -6959, 234525, -2147483648, 2147483647}
+
+	scratch := make([]byte, 5)
+
+	for _, i := range nums {
+
+		putMint32(scratch, i)
+		got := getMint32(scratch)
+		if got != i {
+			t.Errorf("put in %d but got out %d", i, got)
+		}
+
+	}
+
+}
+
 func TestPutGetUnix(t *testing.T) {
 
 	cases := []struct {
